03acc67e011f65c7a7d264908d464d2c1e0cb989
eliu-lotso <421928791@qq.com>
2025-06-10 03:52:50 +0800

    change time ZoneInfo

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from services.weather_service import get_weather_summary
 import notifier
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def main():
     title, summary = get_weather_summary()
+
+    # 重写标题，加上台北时间
+    tpe_now_str = datetime.now(ZoneInfo("Asia/Taipei")).strftime("%Y-%m-%d %H:%M")
+    title = f"今日天气更新（台北时间 {tpe_now_str}）"
+
     notifier.update_rss(title, summary)
     notifier.send_bark(title, summary)
 
